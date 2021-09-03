0b9aa86a1addbcd5c78b6dee0274f0b454b0ca44
Lune <lune9402@naver.com>
2021-09-03 20:29:42 +0900

    어댑터 뼈대 작업을 마무리

--- app/src/main/java/com/example/listview_practice_lune/adaptas/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_practice_lune/adaptas/StudentAdapter.kt
@@ -1,11 +1,35 @@
 package com.example.listview_practice_lune.adaptas
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview_practice_lune.R
 import com.example.listview_practice_lune.datas.Student
 
 class StudentAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+        val inf = LayoutInflater.from(mContext)
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+//        "tem"Row> 임시로 쓰는 한 칸(다른 이름이어도 상관X)
+
+        var tempRow = convertView
+        if (tempRow == null){
+
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
 }
